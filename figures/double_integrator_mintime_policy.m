811c65f148d141b1a0f932d1cd5e71eb8333f17b
Russ Tedrake <russt@mit.edu>
2014-09-16 20:34:21 -0400

    fix double integrator policy

--- figures/double_integrator_mintime_policy.m
+++ figures/double_integrator_mintime_policy.m
@@ -1,4 +1,4 @@
-function mintime_double_int
+function double_integrator_mintime_policy
 
 clf; hold on;
 
@@ -9,11 +9,11 @@ fill([q(1),q,q(end),q(end)],[qdot(end),qdot,qdot(end),qdot(end)],MITgray,'EdgeCo
 
 q = linspace(0,10,100);
 qdot = -sign(q).*sqrt(2*sign(q).*q);
-plot(q,qdot,'LineWidth',2,'Color',.6*MITred);
+plot(q,qdot,'LineWidth',2,'Color',.6*MITgray);
 
 q = linspace(-10,0,100);
 qdot = -sign(q).*sqrt(2*sign(q).*q);
-plot(q,qdot,'LineWidth',2,'Color',.6*MITgray);
+plot(q,qdot,'LineWidth',2,'Color',.6*MITred);
 
 h = legend('u=-1','u=1');
 set(h,'FontName','Times','FontSize',16);
@@ -24,6 +24,6 @@ addXYAxis;
 text(9.5,-0.6,'$q$','Interpreter','latex','FontName','Times','FontSize',20);
 text(-.6,.9*qdot(1),'$\dot{q}$','Interpreter','latex','FontName','Times','FontSize',20);
 
-export2svg('mintime_double_int');
+export2svg(mfilename);
 
 % to do: add arrowheads to the lines?
