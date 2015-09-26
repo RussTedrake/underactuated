4ea625e26d536e10bb707439cfdfc50cbddf472b
Russ Tedrake <russt@mit.edu>
2015-09-26 05:21:32 -0400

    added cygwin install

--- install_prereqs.sh
+++ install_prereqs.sh
@@ -6,12 +6,17 @@ case $1 in
   ("macports")
     pip install beautifulsoup4 html5lib ;;
   ("ubuntu")
-    apt-get install python-bs4 python-html5lib ;;
+      apt-get install python-bs4 python-html5lib ;;
+  ("cygwin")
+      cygwin-setup -q -P python python-setuptools 
+      /usr/bin/easy_install pip 
+      pip install beautifulsoup4 html5lib ;;
   (*)
     echo "Usage: ./install_prereqs.sh package_manager"
     echo "where package_manager is one of the following: "
     echo "  homebrew"
     echo "  macports"
     echo "  ubuntu"
+    echo "  cygwin"
     exit 1 ;;
 esac
