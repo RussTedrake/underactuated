e991123cd896580644452dc8d16cbf77af28b117
Russ Tedrake <russt@mit.edu>
2018-01-23 09:32:03 -0500

    first pass at constant torque demo

--- src/pendulum_visualizer.py
+++ src/pendulum_visualizer.py
@@ -1,49 +1,71 @@
+# -*- coding: utf8 -*-
+
 import numpy as np
 import math
 import matplotlib.animation as animation
 import matplotlib.pyplot as plt
+import pydrake
+from pydrake.systems.framework import (
+    DiagramBuilder,
+    LeafSystem,
+    PortDataType,
+    )
 
-class PendulumVisualizer:
-    a1 = 0.75;  
-    ac1 = 0.75;
-    av = np.linspace(0,math.pi,20);
-    rb = .03; 
-    hb=.07;
-    aw = .01;
-    base_x = rb*np.concatenate(([1], np.cos(av), [-1]));
-    base_y = np.concatenate(([-hb], rb*np.sin(av), [-hb]));
-    arm_x = np.concatenate((aw*np.sin(av-math.pi/2), aw*np.sin(av+math.pi/2)));
-    arm_y = np.concatenate((aw*np.cos(av-math.pi/2), -a1+aw*np.cos(av+math.pi/2)));
-    fig = plt.figure(25);
-    ax = plt.axes();
-    ax.axis('equal');
-    ax.axis('off');
-    ax.set_xlim([-1.2,1.2]);
-    ax.set_ylim([-1.2,1.2]);
-    base = plt.fill(base_x,base_y,zorder=1,color=(.3, .6, .4),edgecolor='k');
-    # arm_x and arm_y are closed (last element == first element), but don't pass the 
-    # last element to the fill command, because it gets closed anyhow (and we want the
-    # sizes to match for the update).
-    arm = plt.fill(arm_x[0:-1],arm_y[0:-1],zorder=0,color=(.9, .1, 0),edgecolor='k');
-    center_of_mass = plt.plot(0,-ac1,zorder=1,color='b',marker='o',markersize=14);
-    
+class PendulumVisualizer(LeafSystem):
+    a1 = 0.75
+    ac1 = 0.75
+    av = np.linspace(0,math.pi,20)
+    rb = .03
+    hb=.07
+    aw = .01
+    base_x = rb*np.concatenate(([1], np.cos(av), [-1]))
+    base_y = np.concatenate(([-hb], rb*np.sin(av), [-hb]))
+    arm_x = np.concatenate((aw*np.sin(av-math.pi/2), aw*np.sin(av+math.pi/2)))
+    arm_y = np.concatenate((aw*np.cos(av-math.pi/2), -a1+aw*np.cos(av+math.pi/2)))
+
+    def __init__(self):
+        LeafSystem.__init__(self)
+        self.set_name('pendulum_visualizer')
+        self._DeclareInputPort(PortDataType.kVectorValued, 2)
+        self._DeclarePeriodicPublish(0.03,0.0)
+
+        (self.fig, ax) = plt.subplots()
+        ax.axis('equal')
+        ax.axis('off')
+        ax.set_xlim([-1.2, 1.2])
+        ax.set_ylim([-1.2, 1.2])
+        self.base = ax.fill(self.base_x, self.base_y, zorder=1, color=(.3, .6, .4), edgecolor='k')
+        # arm_x and arm_y are closed (last element == first element), but don't pass the
+        # last element to the fill command, because it gets closed anyhow (and we want the
+        # sizes to match for the update).
+        self.arm = ax.fill(self.arm_x[0:-1], self.arm_y[0:-1], zorder=0, color=(.9, .1, 0), edgecolor='k')
+        self.center_of_mass = ax.plot(0, -self.ac1, zorder=1, color='b', marker='o', markersize=14)
+        self.draw(0)
+        self.fig.show()
+        self.ax = ax
+
+    def _DoPublish(self, context, event):
+        theta = self.EvalVectorInput(context, 0).get_value()[0]
+        self.draw(theta)
+#        self.fig.canvas.draw()
+        plt.pause(0.0000001)
+
     def draw(self, theta):
-        path = self.arm[0].get_path();
-        path.vertices[:,0] = self.arm_x*math.cos(theta)-self.arm_y*math.sin(theta);
-        path.vertices[:,1] = self.arm_x*math.sin(theta)+self.arm_y*math.cos(theta);
-        self.center_of_mass[0].set_data(self.ac1*math.sin(theta),-self.ac1*math.cos(theta));
-        print theta
-        
+        path = self.arm[0].get_path()
+        path.vertices[:,0] = self.arm_x*math.cos(theta)-self.arm_y*math.sin(theta)
+        path.vertices[:,1] = self.arm_x*math.sin(theta)+self.arm_y*math.cos(theta)
+        self.center_of_mass[0].set_data(self.ac1*math.sin(theta),-self.ac1*math.cos(theta))
+
     def animation_update(self, i):
-        self.draw(math.sin(0.03*i));
+        self.draw(math.sin(0.03*i))
 
     def animate(self, log):
         # log - a reference to a pydrake.systems.primitives.SignalLogger that
         # constains the pendulum state, or pendulum output (the first element
         # should be the pendulum's theta) after running a simulation.
-        t = log.sample_times();
-        x = log.data();
-        ani = animation.FuncAnimation(self.fig, self.animation_update, 500, interval=25, repeat=False);
+        t = log.sample_times()
+        x = log.data()
+        ani = animation.FuncAnimation(self.fig, self.animation_update, 500, interval=25, repeat=False)
         return ani
 
 
