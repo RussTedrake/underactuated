42ebf3246c6e4792f9d555e7b1c3526ff17da2de
Russ Tedrake <russt@mit.edu>
2018-01-31 06:27:35 -0500

    cleanup modeling chapter.  lots more to do.  missing some bindings for discrete time systems even from current VectorSystem PR in wait

--- src/simple_continuous_time_system.py
+++ src/simple_continuous_time_system.py
@@ -1,8 +1,4 @@
-import pydrake
-from pydrake.systems.framework import (DiagramBuilder, VectorSystem)
-from pydrake.systems.primitives import SignalLogger
-from pydrake.systems.analysis import Simulator
-import matplotlib.pyplot as plt
+from pydrake.systems.framework import VectorSystem
 
 # Define the system.
 class SimpleContinuousTimeSystem(VectorSystem):
@@ -12,37 +8,10 @@ class SimpleContinuousTimeSystem(VectorSystem):
             1)                           # One output.
         self._DeclareContinuousState(1)  # One state variable.
 
-    # xdot = -x + x^3
+    # xdot(t) = -x(t) + x^3(t)
     def _DoCalcVectorTimeDerivatives(self, context, u, x, xdot):
         xdot[:] = -x + x**3
 
-    # y = x
+    # y(t) = x(t)
     def _DoCalcVectorOutput(self, context, u, x, y):
         y[:] = x
-
-
-# Create a simple block diagram containing our system.
-builder = DiagramBuilder()
-system = builder.AddSystem(SimpleContinuousTimeSystem())
-# TODO(russt): add binding then replace the next two lines with
-#   logger = LogOutput(system->get_output_port(0), builder)
-logger = builder.AddSystem(SignalLogger(1))
-builder.Connect(system.get_output_port(0), logger.get_input_port(0))
-diagram = builder.Build()
-
-# Create the simulator.
-simulator = Simulator(diagram)
-
-# Set the initial conditions, x(0).
-state = simulator.get_mutable_context().get_mutable_state()\
-                 .get_mutable_continuous_state().get_mutable_vector()
-state.SetFromVector([0.9])
-
-# Simulate for 10 seconds.
-simulator.StepTo(10)
-
-# Plot the results.
-plt.plot(logger.sample_times(), logger.data().transpose())
-plt.xlabel('t')
-plt.ylabel('x(t)')
-plt.show()
