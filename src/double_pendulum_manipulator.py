25cd248a22cdb8ffa5e4e8d7a27b11bd8955785c
peteflorence <pete.florence@gmail.com>
2018-02-07 00:59:41 -0500

    change all references to double_pendulum.urdf to use the findResource

--- src/double_pendulum_manipulator.py
+++ src/double_pendulum_manipulator.py
@@ -1,7 +1,8 @@
 from pydrake.multibody.rigid_body_tree import RigidBodyTree
 from manipulator_dynamics import *
+import underactuated_utils as utils
 
-tree = RigidBodyTree("double_pendulum.urdf", FloatingBaseType.kFixed);
+tree = RigidBodyTree(utils.findResource("double_pendulum.urdf"), FloatingBaseType.kFixed);
 
 q = (1., 1.)
 v = (0.1, 0.1)
