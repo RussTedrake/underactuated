cb77cb041c503b6de55dfb2e3d2f305431fe46b2
Russ Tedrake <russt@mit.edu>
2018-05-15 06:23:59 -0400

    finish initial van der Pol particles implementation

--- src/van_der_pol/particles.py
+++ src/van_der_pol/particles.py
@@ -21,15 +21,21 @@ class VanDerPolParticles(LeafSystem):
         self._DeclareContinuousState(num_particles, num_particles, 0)
         self._DeclareVectorOutputPort(BasicVector(2*num_particles),
                                       self.CopyStateOut)
+        self.num_particles = num_particles
         self.mu = mu
 
+    # TODO(russt):  SetRandomState to  [-0.1144;2.0578] + 0.01*randn(...)
+
     def _DoCalcTimeDerivatives(self, context, derivatives):
-        q = context.get_continuous_state_vector().GetAtIndex(0)
-        qdot = context.get_continuous_state_vector().GetAtIndex(1)
-        w = self.EvalVectorInput(context, 0).GetAtIndex(0)
-        qddot = -self.mu * (q * q - 1) * qdot - q;
-        derivatives.get_mutable_vector().SetAtIndex(0, qdot)
-        derivatives.get_mutable_vector().SetAtIndex(1, qddot)
+        # TODO(russt):  Update this to get_position/velocity once those are
+        # bound.
+        x = context.get_continuous_state_vector().CopyToVector()
+        q = x[:self.num_particles]
+        qdot = x[self.num_particles:]
+        w = self.EvalVectorInput(context, 0).CopyToVector()
+        qddot = -self.mu * (q * q - 1) * qdot - q + .5 * w
+        derivatives.get_mutable_vector().SetFromVector(np.concatenate((qdot,
+                                                                       qddot)))
 
     # y(t) = x(t)
     def CopyStateOut(self, context, output):
@@ -39,47 +45,36 @@ class VanDerPolParticles(LeafSystem):
 
 
 # Note: This is a candidate for moving to a more central location.
-class HistogramVisualizer(PyPlotVisualizer):
-    def __init__(self, num_samples, bins, xlim, ylim, draw_timestep):
+class Particle2DVisualizer(PyPlotVisualizer):
+    def __init__(self, num_particles, xlim, ylim, draw_timestep):
         PyPlotVisualizer.__init__(self, draw_timestep)
-        for i in range(0, num_samples):
-            self._DeclareInputPort(PortDataType.kVectorValued, 1)
-        self.num_samples = num_samples
-        self.bins = bins
-        self.data = array.array('d',(0,)*num_samples)
-        self.limits = xlim
+        self._DeclareInputPort(PortDataType.kVectorValued, 2*num_particles)
+        self.num_particles = num_particles
         self.ax.set_xlim(xlim)
         self.ax.set_ylim(ylim)
-        self.patches = None
+        zero = array.array('d', (0,)*num_particles)
+        self.lines, = self.ax.plot(zero, zero, 'b.')
 
     def draw(self, context):
-        if (self.patches):
-            t = [p.remove() for p in self.patches]
-        for i in range(0, self.num_samples):
-            self.data[i] = self.EvalVectorInput(context, i).GetAtIndex(0)
-        count, bins, self.patches = self.ax.hist(self.data, bins=self.bins,
-                                                 range=self.limits,
-                                                 density=True, facecolor='b')
+        xy = self.EvalVectorInput(context, 0).CopyToVector()
+        self.lines.set_xdata(xy[:self.num_particles])
+        self.lines.set_ydata(xy[self.num_particles:])
         self.ax.set_title('t = ' + str(context.get_time()))
 
 
 builder = DiagramBuilder()
 
-num_particles = 1000
-num_bins = 100
-xlim = [-1.5, 1.5]
-ylim = [-2, 4.5]
+num_particles = 5000
+xlim = [-2.75, 2.75]
+ylim = [-3.25, 3.25]
 draw_timestep = .25
-visualizer = builder.AddSystem(HistogramVisualizer(num_particles, num_bins,
-                                                   xlim, ylim, draw_timestep))
-x = np.linspace(xlim[0],xlim[1],100)
-visualizer.ax.plot(x, dynamics(x, 0), 'k', linewidth=2)
-
-for i in range(0, num_particles):
-    sys = builder.AddSystem(SimpleStochasticSystem())
-    builder.Connect(sys.get_output_port(0), visualizer.get_input_port(i))
-
+sys = builder.AddSystem(VanDerPolParticles(num_particles))
+visualizer = builder.AddSystem(Particle2DVisualizer(num_particles, xlim,
+                                                    ylim, draw_timestep))
+builder.Connect(sys.get_output_port(0), visualizer.get_input_port(0))
 AddRandomInputs(.1, builder)
+
+# TODO(russt): Plot nominal limit cycle.
 
 diagram = builder.Build()
 simulator = Simulator(diagram)
