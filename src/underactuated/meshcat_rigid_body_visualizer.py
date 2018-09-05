MRBV features: animation time scaling, collision drawing, and coloring fix (#138)
* Remove printout in PRBV

* Allow collision visualization and viz clearing in MRBV

* MRBV material color bug fix

* Missed this line for drawing collisions

* Sim time scaling support in MRBV animation

* Remove clear_vis

Seems out of scope for vanilla MRBV

* Split out RGBA2HEX to util and use in new meshcat viz

* Don't need to test viz of val

* Add a little documentation.

src/underactuated/meshcat_rigid_body_visualizer.py
@@ -42,7 +42,7 @@ from pydrake.all import (
     Simulator,
 )
 
-from underactuated.utils import FindResource
+from underactuated.utils import FindResource, Rgba2Hex
 
 import meshcat
 import meshcat.transformations as tf
@@ -53,12 +53,14 @@ class MeshcatRigidBodyVisualizer(LeafSystem):
                  rbtree,
                  draw_timestep=0.033333,
                  prefix="RBViz",
-                 zmq_url="tcp://127.0.0.1:6000"):
+                 zmq_url="tcp://127.0.0.1:6000",
+                 draw_collision=False):
         LeafSystem.__init__(self)
         self.set_name('meshcat_visualization')
         self.timestep = draw_timestep
         self._DeclarePeriodicPublish(draw_timestep, 0.0)
         self.rbtree = rbtree
+        self.draw_collision = draw_collision
 
         self._DeclareInputPort(PortDataType.kVectorValued,
                                self.rbtree.get_num_positions() +
@@ -84,9 +86,13 @@ class MeshcatRigidBodyVisualizer(LeafSystem):
             # function gets bound in pydrake.
             body_name = body.get_name() + ("(%d)" % body_i)
 
-            visual_elements = body.get_visual_elements()
-            this_body_patches = []
-            for element_i, element in enumerate(visual_elements):
+            if self.draw_collision:
+                draw_elements = [self.rbtree.FindCollisionElement(k)
+                                 for k in body.get_collision_element_ids()]
+            else:
+                draw_elements = body.get_visual_elements()
+
+            for element_i, element in enumerate(draw_elements):
                 element_local_tf = element.getLocalTransform()
                 if element.hasGeometry():
                     geom = element.getGeometry()
@@ -118,19 +124,13 @@ class MeshcatRigidBodyVisualizer(LeafSystem):
                               geom.getShape(), " IGNORED"
                         continue
 
-                    def rgba2hex(rgb):
-                        ''' Turn a list of R,G,B elements (any indexable
-                        list of >= 3 elements will work), where each element
-                        is specified on range [0., 1.], into the equivalent
-                        24-bit value 0xRRGGBB. '''
-                        val = 0.
-                        for i in range(3):
-                            val += (256**(2 - i)) * (255. * rgb[i])
-                        return val
+                    rgba = [1., 0.7, 0., 1.]
+                    if not self.draw_collision:
+                        rgba = element.getMaterial()
                     self.vis[self.prefix][body_name][str(element_i)]\
                         .set_object(meshcat_geom,
                                     meshcat.geometry.MeshLambertMaterial(
-                                        color=rgba2hex(element.getMaterial())))
+                                        color=Rgba2Hex(rgba)))
                     self.vis[self.prefix][body_name][str(element_i)].\
                         set_transform(element_local_tf)
 
@@ -141,6 +141,7 @@ class MeshcatRigidBodyVisualizer(LeafSystem):
         ''' Evaluates the robot state and draws it.
             Can be passed either a raw state vector, or
             an input context.'''
+
         if isinstance(context, Context):
             positions = self.EvalVectorInput(context, 0).get_value()[0:self.rbtree.get_num_positions()]  # noqa
         else:
@@ -151,11 +152,18 @@ class MeshcatRigidBodyVisualizer(LeafSystem):
         body_fill_index = 0
         for body_i in range(self.rbtree.get_num_bodies()-1):
             tf = self.rbtree.relativeTransform(kinsol, 0, body_i+1)
-            body_name = self.rbtree.get_body(body_i+1).get_name() \
-                + ("(%d)" % body_i)
-            self.vis[self.prefix][body_name].set_transform(tf)
+            body = self.rbtree.get_body(body_i+1)
+            # Don't try to update the transform of geometry
+            # that doesn't exist.
+            if ((self.draw_collision and
+                    len(body.get_collision_element_ids()) > 0)
+                or
+                (not self.draw_collision and
+                    len(body.get_visual_elements()) > 0)):
+                body_name = body.get_name() + ("(%d)" % body_i)
+                self.vis[self.prefix][body_name].set_transform(tf)
 
-    def animate(self, log, resample=True):
+    def animate(self, log, resample=True, time_scaling=1.0):
         # log - a reference to a pydrake.systems.primitives.SignalLogger that
         # contains the plant state after running a simulation.
         # resample -- should we do a resampling operation to make
@@ -183,9 +191,16 @@ class MeshcatRigidBodyVisualizer(LeafSystem):
         def animate_update(i):
             self.draw(x[:, i])
 
+        # Keep track of real elapsed vs sim elapsed time when playing back
+        # animation, and sleep whenever we get ahead of the simulation
+        # time (scaled by the time scaling factor).
+        start_time = time.time()
+        sim_start_time = t[0]
         for i in range(t.shape[0]):
             animate_update(i)
-            time.sleep(self.timestep)
+            sim_time = t[i] - sim_start_time
+            real_time = time.time() - start_time
+            time.sleep(max(0, sim_time/time_scaling - real_time))
 
 
 def setupPendulumExample():
@@ -238,7 +253,7 @@ def setupValkyrieExample():
                       [0., 0., 1., 0.],
                       [0., 0., 0., 1.]],
                      dtype=np.float64)
-    pbrv = MeshcatRigidBodyVisualizer(rbt)
+    pbrv = MeshcatRigidBodyVisualizer(rbt, draw_collision=True)
     return rbt, pbrv
 
 
@@ -344,8 +359,6 @@ if __name__ == "__main__":
             state.SetFromVector(initial_state)
 
         simulator.StepTo(args.duration)
-
-        print(state.CopyToVector())
 
         if (args.animate):
             # Generate an animation of whatever happened