35219e6de23ad14f27d85d098753f8a2a8f557b7
Russ Tedrake <russt@mit.edu>
2019-03-28 21:59:11 -0400

    update rimless wheel dircol. depends on https://github.com/robotlocomotion/drake/pull/11076

--- src/rimless_wheel/dircol_limitcycle.py
+++ src/rimless_wheel/dircol_limitcycle.py
@@ -1,37 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from pydrake.examples.pendulum import PendulumPlant, PendulumParams
-from pydrake.examples.rimless_wheel import RimlessWheelParams
+from pydrake.examples.rimless_wheel import RimlessWheel
 from pydrake.all import DirectCollocation, Solve
 
-# This is just a placeholder... until we have more support for hybrid
-# trajectory optimization, we'll do optimization on an equivalent pendulum.
-plant = PendulumPlant()
+plant = RimlessWheel()
 context = plant.CreateDefaultContext()
 
-rw_params = RimlessWheelParams()
-alpha = np.pi / rw_params.number_of_spokes()
-pendulum_params = context.get_numeric_parameter(0)
-pendulum_params.set_mass(rw_params.mass())
-pendulum_params.set_length(rw_params.length())
-pendulum_params.set_damping(0.)
+params = context.get_numeric_parameter(0)
+slope = params.slope()
+alpha = np.pi / params.number_of_spokes()
 
-dircol = DirectCollocation(plant, context, num_time_samples=11,
-                           minimum_timestep=0.01, maximum_timestep=0.1)
+dircol = DirectCollocation(plant, context, num_time_samples=15,
+                           minimum_timestep=0.01, maximum_timestep=0.1,
+                           assume_non_continuous_states_are_fixed=True)
 
 dircol.AddEqualTimeIntervalsConstraints()
 
-dircol.AddConstraintToAllKnotPoints(dircol.state()[0] >=
-                                    np.pi+rw_params.slope()-alpha)
-dircol.AddConstraintToAllKnotPoints(dircol.state()[0] <=
-                                    np.pi+rw_params.slope()+alpha)
+dircol.AddConstraintToAllKnotPoints(dircol.state()[0] >= slope - alpha)
+dircol.AddConstraintToAllKnotPoints(dircol.state()[0] <= slope + alpha)
 
-dircol.AddConstraint(dircol.initial_state()[0] ==
-                     np.pi+rw_params.slope()-alpha)
-
-dircol.AddConstraint(dircol.final_state()[0] ==
-                     np.pi+rw_params.slope()+alpha)
+dircol.AddConstraint(dircol.initial_state()[0] == slope - alpha)
+dircol.AddConstraint(dircol.final_state()[0] == slope + alpha)
 
 dircol.AddConstraint(dircol.initial_state()[1] ==
                      dircol.final_state()[1]*np.cos(2*alpha))
@@ -53,18 +43,18 @@ ax.plot([x_knots[0, 0], x_knots[0, -1]], [x_knots[1, 0], x_knots[1, -1]],
 # Plot the energy contours.
 nq = 151
 nqd = 151
-mgl = pendulum_params.mass()*pendulum_params.gravity()*pendulum_params.length()
-q = np.linspace(np.pi - 0.5, np.pi + 0.5, nq)
+mgl = params.mass()*params.gravity()*params.length()
+q = np.linspace(-0.5, 0.5, nq)
 qd = np.linspace(-.5, 2, nqd)
 Q, QD = np.meshgrid(q, qd)
-Energy = .5*pendulum_params.mass()*pendulum_params.length()**2*QD**2 + \
-         mgl*(1-np.cos(Q))
+Energy = .5*params.mass()*params.length()**2*QD**2 + \
+         mgl*np.cos(Q)
 ax.contour(Q, QD, Energy, alpha=0.5, linestyles='dashed',
            colors='black', linewidths=0.5)
 
 ax.set_xlabel('theta')
 ax.set_ylabel('thetadot')
-ax.axis([np.pi - 0.5, np.pi + 0.5, 0, 2])
+ax.axis([-0.5, 0.5, 0, 2])
 ax.set_title('Limit Cycle of the Rimless Wheel (w/ contours of '
              'constant energy)')
 
