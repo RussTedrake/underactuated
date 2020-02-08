f47d32e3d36ffd79caeab66e0911eef508fd279d
Russ Tedrake <russt@mit.edu>
2020-02-07 23:25:56 -0500

    setup header and footer; move navigation logic to install script

--- chapters.js
+++
@@ -1,1 +0,0 @@
-var chapter_ids = ["intro", "pend", "acrobot", "simple_legs", "humanoids", "stochastic", "dp", "lqr", "lyapunov", "trajopt", "planning", "feedback_motion_planning", "policy_search", "robust", "output_feedback", "limit_cycles", "contact", "rl_policy_search", "drake", "multibody", "optimization", "playbook"];
