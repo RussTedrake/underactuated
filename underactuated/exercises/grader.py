11dbb5a73ccd7f9bce26266d06809ccfe5dd244c
wilko <wilkos@mit.edu>
2020-02-15 18:46:05 -0500

    Deactivated pickling

--- underactuated/exercises/grader.py
+++ underactuated/exercises/grader.py
@@ -2,6 +2,10 @@ import unittest
 from gradescope_utils.autograder_utils.json_test_runner import JSONTestRunner
 import dill
 import json
+from runpy import run_module
+import nbformat
+from nbconvert import PythonExporter
+import os
 
 class Grader:
 
@@ -9,31 +13,75 @@ class Grader:
         pass
 
     @staticmethod
-    def grade_from_dills(notebook_dill_list, test_cases_list, results_json, notebook_ipynb_list=None):
+    def run_notebook(notebook_ipynb):
+
+        banned_commands = [
+            'start_recording',
+            'stop_recording',
+            'get_recording_as_animation',
+            'HTML',
+        ]
+
+        ipynb = json.load(open(notebook_ipynb))
+
+        for cell in ipynb['cells']:
+            # erase test cells to prevent recursion of tests
+            if any('## TEST ##' in line for line in cell['source']):
+                cell['source'] = []
+            # filter out all the lines with banned commands
+            if banned_commands is not None:
+                cell['source'] = [line for line in cell['source'] if not any(command in line for command in banned_commands)]
+
+        exporter = PythonExporter()
+        source, meta = exporter.from_notebook_node(
+            nbformat.reads(json.dumps(ipynb), nbformat.NO_CONVERT)
+            )
+        with open('./cleaned_notebook.py', 'w') as fh:
+            fh.write(source)
+        
+        notebook_locals =  run_module('cleaned_notebook')
+        # os.system('rm cleanded_notebook.py')
+        return notebook_locals
+
+
+    @staticmethod
+    def grade_from_dills(notebook_dill_list, test_cases_list, results_json, notebook_ipynb_list):
         '''Reading notebook_locals from dills and grading on the corresponding test_cases_list. 
         Optional check for existence of student notebooks in notebook_ipynb_list.
         Result is written into results_json'''
 
-         # Read in all dill files into notebook_locals_list
-        try:
-            notebook_locals_list = []
-            for notebook_dill in notebook_dill_list:
-                with open(notebook_dill, "rb") as dill_file:
-                    notebook_locals_list.append(dill.load(dill_file))
-        except Exception as e:
-            Grader.global_fail_with_error_message("Missing file: " + notebook_dill + ', ' + str(e), results_json)
-            raise
+        #  # Read in all dill files into notebook_locals_list
+        # try:
+        #     notebook_locals_list = []
+        #     for notebook_dill in notebook_dill_list:
+        #         with open(notebook_dill, "rb") as dill_file:
+        #             notebook_dill_read = dill.load(dill_file)
+        #         notebook_locals_list.append(notebook_dill_read)
+        # except Exception as e:
+        #     Grader.global_fail_with_error_message("Missing file: " + notebook_dill + ', ' + str(e), results_json)
+        #     raise
+
+        # # If there are notebooks to be submitted, read in all notebook files into notebook_list
+        # if notebook_ipynb_list:
+        #     try:
+        #         notebook_list = []
+        #         for notebook_ipynb in notebook_ipynb_list:
+        #             with open(notebook_ipynb, "rb") as notebook_file:
+        #                 notebook_list.append(json.load(notebook_file))
+        #     except Exception as e:
+        #         Grader.global_fail_with_error_message("Missing file: " + notebook_ipynb + ', ' + str(e), results_json)
+        #         raise
 
         # If there are notebooks to be submitted, read in all notebook files into notebook_list
-        if notebook_ipynb_list:
-            try:
-                notebook_list = []
-                for notebook_ipynb in notebook_ipynb_list:
-                    with open(notebook_ipynb, "rb") as notebook_file:
-                        notebook_list.append(json.load(notebook_file))
-            except Exception as e:
-                Grader.global_fail_with_error_message("Missing file: " + notebook_ipynb + ', ' + str(e), results_json)
-                raise
+        # Grader.run_notebook(notebook_ipynb_list[0])
+
+        try:
+            notebook_locals_list = []
+            for notebook_ipynb in notebook_ipynb_list:
+                notebook_locals_list.append(Grader.run_notebook(notebook_ipynb))
+        except Exception as e:
+            Grader.global_fail_with_error_message("Missing file: " + notebook_ipynb + ', ' + str(e), results_json)
+            raise
 
         # Grade notebook_locals_list on test_cases_list
         Grader.grade_output(test_cases_list, notebook_locals_list, results_json)
