b85c05ba2f6cbfc3fcab6ed6e0d32b9f1f389558
wilko <wilkos@mit.edu>
2020-02-15 08:23:05 -0500

    Fixed read in bug for notebooks

--- underactuated/exercises/grader.py
+++ underactuated/exercises/grader.py
@@ -14,23 +14,28 @@ class Grader:
         Optional check for existence of student notebooks in notebook_ipynb_list.
         Result is written into results_json'''
 
+         # Read in all dill files into notebook_locals_list
         try:
-            # Read in all dill files into notebook_locals_list
             notebook_locals_list = []
-            for notebook_dill in zip(notebook_dill_list):
+            for notebook_dill in notebook_dill_list:
                 with open(notebook_dill, "rb") as dill_file:
                     notebook_locals_list.append(dill.load(dill_file))
-
-            # If there are notebooks to be submitted, read in all notebook files into notebook_list
-            if notebook_ipynb_list:
-                notebook_list = []
-                for notebook_dill in zip(notebook_dill_list, test_cases_list):
-                    with open(notebook_dill, "rb") as notebook_file:
-                        notebook_list.append(json.load(notebook_file))
-
         except Exception as e:
-            Grader.global_fail_with_error_message("A file is missing: " + str(e), results_json)
-        raise
+            Grader.global_fail_with_error_message("Missing file: " + notebook_dill + ', ' + str(e), results_json)
+            raise
+
+        # If there are notebooks to be submitted, read in all notebook files into notebook_list
+        if notebook_ipynb_list:
+            try:
+                notebook_list = []
+                for notebook_ipynb in notebook_ipynb_list:
+                    with open(notebook_ipynb, "rb") as notebook_file:
+                        notebook_list.append(json.load(notebook_file))
+            except Exception as e:
+                Grader.global_fail_with_error_message("Missing file: " + notebook_ipynb + ', ' + str(e), results_json)
+                raise
+
+
 
         # Grade notebook_locals_list on test_cases_list
         Grader.grade_output(test_cases_list, notebook_locals_list, results_json)
