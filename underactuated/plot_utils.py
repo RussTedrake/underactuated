b208503daff5d52decd7b06beb376fbd8e874809
Russ Tedrake <russt@mit.edu>
2020-03-15 10:35:38 -0400

    Add lyapunov examples using the drake RegionOfAttraction method. (#332)
    time-reversed van der Pol, "star convexity".
    Generalizes the vector field plotting
    Uses improvements to Trajectory access methods i've pushed into drake.

--- underactuated/plot_utils.py
+++ underactuated/plot_utils.py
@@ -1,5 +1,8 @@
+from copy import copy
 import numpy as np
 import matplotlib.pyplot as plt
+
+from pydrake.all import System
 
 
 def plot_2d_phase_portrait(f, x1lim=(-1, 1), x2lim=(-1, 1), n=100j, **kwargs):
@@ -22,12 +25,23 @@ def plot_2d_phase_portrait(f, x1lim=(-1, 1), x2lim=(-1, 1), n=100j, **kwargs):
     """
     # grid state space, careful here x2 before x1
     X1, X2 = np.mgrid[x1lim[0]:x1lim[1]:n, x2lim[0]:x2lim[1]:n]
-    X1d, X2d = f([X1, X2])
+    if isinstance(f, System):
+        context = f.CreateDefaultContext()
+        X1d = copy(X1)
+        X2d = copy(X2)
+        for i in range(X1.shape[0]):
+            for j in range(X1.shape[1]):
+                context.SetContinuousState([X1[i, j], X2[i, j]])
+                deriv = f.EvalTimeDerivatives(context).CopyToVector()
+                X1d[i, j] = deriv[0]
+                X2d[i, j] = deriv[1]
+    else:
+        X1d, X2d = f([X1, X2])
 
     # color the streamlines according to the magnitude of xdot
     color = np.sqrt(X1d**2 + X2d**2)
 
-    # phase portrait (annoing input format of streamplot)
+    # phase portrait (annoying input format of streamplot)
     strm = plt.streamplot(X1.T[0], X2[0], X1d.T, X2d.T, color=color.T, **kwargs)
 
     # colorbar on the right that measures the magnitude of xdot
