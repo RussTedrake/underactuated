d762289c3a308bdb08839bf9189726eb71f07dc6
Tobia Marcucci <tobia.marcucci@gmail.com>
2020-04-01 17:30:51 -0400

    Implements Russ' comments on the PR.

--- underactuated/exercises/simple_legs/one_d_hopper/test_one_d_hopper.py
+++ underactuated/exercises/simple_legs/one_d_hopper/test_one_d_hopper.py
@@ -4,7 +4,7 @@ from gradescope_utils.autograder_utils.decorators import weight
 import numpy as np
 
 
-class TestHopper(unittest.TestCase):
+class TestOneDHopper(unittest.TestCase):
 
     def __init__(self, test_name, notebook_locals):
         super().__init__(test_name)
