40ca54c3fe5eab4ee05ea9525af9853a9827fdd8
Eric Chen <echen9898@gmail.com>
2021-03-04 06:42:33 -0500

    Added fitted double integrator exercise (#418)
    * Added fitted double integrator exercise
    
    * Updated dp textbook chapter with exercise listing
    
    * Added exercise notebook
    
    * Added exercise autograder test file
    
    * Addressed first round of build fails
    
    * Fixed formatting issues
    
    * Adressed first round of comments
    
    * Switched from barycentric mesh policy to a vector system
    
    * Reworded written questions
    
    * Adressed second round of comments
    
    * Modified .py file for double integrator visualization
    
    * Updated notebook to reflect this change
    
    * Updated second written question
    
    * Fixed formatting on double integrator .py file
    
    * Formatting issue
    
    * Took out 4th part of fitted value iteration exercise
    
    * Took out the 4th problem on convergence
    
    * Reworded the 3rd part on stochastic transitions
    
    * Added double_integrator.py dependency in bazel

---
+++ underactuated/exercises/dp/fitted_double_integrator/test_fitted_double_integrator.py
@@ -0,0 +1,78 @@
+import unittest
+import timeout_decorator
+from gradescope_utils.autograder_utils.decorators import weight
+
+
+class TestFittedDoubleIntegrator(unittest.TestCase):
+
+    def __init__(self, test_name, notebook_locals):
+        super().__init__(test_name)
+        self.notebook_locals = notebook_locals
+
+    @weight(4)
+    @timeout_decorator.timeout(20.)
+    def test_final_losses(self):
+        """Test model training"""
+        # note: all prints here go to the output item in the json file
+
+        # the loss must be close to the correct value
+        final_loss_min_time = self.notebook_locals['final_loss_min_time']
+        self.assertLessEqual(
+            final_loss_min_time,
+            0.0003,
+            msg='Min time loss is < than upper bound for the correct value')
+        self.assertGreaterEqual(
+            final_loss_min_time,
+            0.0001,
+            msg='Min time loss is > than lower bound for the correct value')
+
+        final_loss_quadratic = self.notebook_locals['final_loss_quadratic']
+        self.assertLessEqual(
+            final_loss_quadratic,
+            0.009,
+            msg='Quadratic loss is < than upper bound for the correct value')
+        self.assertGreaterEqual(
+            final_loss_quadratic,
+            0.002,
+            msg='Quadratic loss is > than lower bound for the correct value')
+
+    @weight(4)
+    @timeout_decorator.timeout(20.)
+    def test_policy(self):
+        """Test closed form policy"""
+        # note: all prints here go to the output item in the json file
+        final_state_min_time = self.notebook_locals['final_state_min_time']
+        min_time_position, min_time_velocity = final_state_min_time
+        self.assertLessEqual(
+            min_time_position,
+            0.6,
+            msg='Minimum time policy final position is too large')
+        self.assertGreaterEqual(
+            min_time_position,
+            0.0,
+            msg='Minimum time policy final position is too small')
+        self.assertLessEqual(
+            min_time_velocity,
+            0.03,
+            msg='Minimum time policy final velocity is too large')
+        self.assertGreaterEqual(
+            min_time_velocity,
+            -0.03,
+            msg='Minimum time policy final velocity is too small')
+
+        final_state_quadratic = self.notebook_locals['final_state_quadratic']
+        quadratic_position, quadratic_velocity = final_state_quadratic
+        self.assertLessEqual(quadratic_position,
+                             0.6,
+                             msg='Quadratic policy final position is too large')
+        self.assertGreaterEqual(
+            quadratic_position,
+            0.0,
+            msg='Quadratic policy final position is too small')
+        self.assertLessEqual(quadratic_velocity,
+                             0.03,
+                             msg='Quadratic policy final velocity is too large')
+        self.assertGreaterEqual(
+            quadratic_velocity,
+            -0.03,
+            msg='Quadratic policy final velocity is too small')
