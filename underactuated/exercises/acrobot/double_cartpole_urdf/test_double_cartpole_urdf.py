4f295e0d97afb110e5620ed60468e849e5dd8bf1
AJ <ajm4@mit.edu>
2021-03-04 13:25:28 -0500

    added testing file for 3.2 and made fixes for PR

---
+++ underactuated/exercises/acrobot/double_cartpole_urdf/test_double_cartpole_urdf.py
@@ -0,0 +1,61 @@
+import unittest
+import timeout_decorator
+from gradescope_utils.autograder_utils.decorators import weight
+import numpy as np
+
+
+class TestDoubleCartPoleURDF(unittest.TestCase):
+
+    def __init__(self, test_name, notebook_locals):
+        super().__init__(test_name)
+        self.notebook_locals = notebook_locals
+
+
+    @weight(2)
+    @timeout_decorator.timeout(1.)
+    def test_q1_get_single_pendulum_urdf_parameters(self):
+        """Test the single_pendulum_urdf_parameters"""
+        # note: all prints here go to the output item in the json file
+        get_single_pendulum_urdf_parameters = self.notebook_locals['get_single_pendulum_urdf_parameters']
+        urdf_parameters = get_single_pendulum_urdf_parameters()
+        a = 0
+        for x in get_single_pendulum_urdf_parameters():
+            a += hash(tuple(x))
+        self.assertEqual(a, -3713082714464628219,
+                         "Incorrect single pendulum urdf parameters.")
+
+    @weight(1)
+    @timeout_decorator.timeout(1.)
+    def test_q2_create_single_cartpole_urdf(self):
+        """Test the single_pendulum_urdf string"""
+        # note: all prints here go to the output item in the json file
+        get_single_pendulum_urdf_parameters = self.notebook_locals['get_single_pendulum_urdf_parameters']
+        create_cartpole_urdf = self.notebook_locals['create_cartpole_urdf']
+        a = hash(create_cartpole_urdf(get_single_pendulum_urdf_parameters()))
+        self.assertEqual(a, -1515303374163843506,
+                         "Incorrect single pendulum urdf string. Did you modify the create_cartpole_urdf function?")
+
+
+    @weight(4)
+    @timeout_decorator.timeout(1.)
+    def test_q3_get_double_pendulum_urdf_parameters(self):
+        """Test the double_pendulum_urdf_parameters"""
+        # note: all prints here go to the output item in the json file
+        get_double_pendulum_urdf_parameters = self.notebook_locals['get_double_pendulum_urdf_parameters']
+        urdf_parameters = get_double_pendulum_urdf_parameters()
+        a = 0
+        for x in get_double_pendulum_urdf_parameters():
+            a += hash(tuple(x))
+        self.assertEqual(a, -7426164346401008863,
+                         "Incorrect double pendulum urdf parameters.")
+
+    @weight(1)
+    @timeout_decorator.timeout(1.)
+    def test_q4_create_double_cartpole_urdf(self):
+        """Test the double_pendulum_urdf_parameters"""
+        # note: all prints here go to the output item in the json file
+        get_double_pendulum_urdf_parameters = self.notebook_locals['get_double_pendulum_urdf_parameters']
+        create_cartpole_urdf = self.notebook_locals['create_cartpole_urdf']
+        a = hash(create_cartpole_urdf(get_double_pendulum_urdf_parameters()))
+        self.assertEqual(a, -4292107062198214891,
+                         "Incorrect double pendulum urdf string. Did you modify the create_cartpole_urdf function?")
