a7cc94d0b929f74812ca5abea784e92e5a22c635
Russ Tedrake <russt@mit.edu>
2021-03-06 10:19:06 -0500

    update shas

--- scripts/setup/jupyter_setup.py
+++ scripts/setup/jupyter_setup.py
@@ -20,6 +20,6 @@ def setup_drake():
 
 def setup_underactuated():
     new_setup.setup_underactuated(
-        underactuated_sha='1dca1b915977aeec9f327d61aaeac4cfb9c6b408',
+        underactuated_sha='2a15bde1136495c41226a92dfdebf3b3b53cf830',
         drake_version='0.27.0',
         drake_build='release')
