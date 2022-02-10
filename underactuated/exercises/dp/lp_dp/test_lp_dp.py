7c74324c2ff953ccec028c94c04fde4425a1f6a0
Russ Tedrake <russt@mit.edu>
2022-02-10 06:07:09 -0500

    LP for DP problem from Lu

---
+++ underactuated/exercises/dp/lp_dp/test_lp_dp.py
@@ -0,0 +1,27 @@
+import unittest
+import numpy as np
+import timeout_decorator
+from gradescope_utils.autograder_utils.decorators import weight
+
+
+class Testlpdp(unittest.TestCase):
+
+    def __init__(self, test_name, notebook_locals):
+        super().__init__(test_name)
+        self.notebook_locals = notebook_locals
+
+    @weight(8)
+    @timeout_decorator.timeout(1.)
+    def test_J_from_lp(self):
+        """Test optimal cost-to-go from Linear Program"""
+        # note: all prints here go to the output item in the json file
+        J_value = self.notebook_locals['J_value']
+
+        J = np.load("J.npy")
+
+        diff = np.abs(J - J_value)
+
+        self.assertTrue(
+            (diff <= 1e-6).all(),
+            msg='|J - J*| > 1e-6',
+        )
