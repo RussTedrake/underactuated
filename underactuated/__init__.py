4112491a808a9a82a31d490c953702cf2f4ade1b
Russ Tedrake <russt@mit.edu>
2022-02-26 21:05:34 -0500

    fix flaky tests

--- underactuated/__init__.py
+++ underactuated/__init__.py
@@ -1,5 +1,7 @@
 from .jupyter import running_as_notebook
 from .pyplot_visualizer import SliderSystem
 from .multibody import ManipulatorDynamics
-from .utils import FindResource, running_as_test
+# Note: can't import running_as_test here.  Will get a copy instead of a
+# reference.
+from .utils import FindResource
 from .plot_utils import plot_2d_phase_portrait
