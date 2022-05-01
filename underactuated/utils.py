7d5114520a4e25afa13037668c3af02b84013436
Russ Tedrake <russt@mit.edu>
2022-05-01 16:07:00 -0400

    update littledog MakeNamedView* logic

--- underactuated/utils.py
+++ underactuated/utils.py
@@ -1,5 +1,7 @@
 import os
 
+from pydrake.all import (JointIndex, namedview)
+
 running_as_test = False
 
 
@@ -22,3 +24,54 @@ def Rgba2Hex(rgb):
     for i in range(3):
         val += (256**(2 - i)) * int(255 * rgb[i])
     return val
+
+
+# TODO(russt): promote these to drake (and make a version with model_instance)
+
+
+def MakeNamedViewPositions(mbp, view_name, add_suffix_if_single_position=False):
+    names = [None] * mbp.num_positions()
+    for ind in range(mbp.num_joints()):
+        joint = mbp.get_joint(JointIndex(ind))
+        if joint.num_positions() == 1 and not add_suffix_if_single_position:
+            names[joint.position_start()] = joint.name()
+        else:
+            for i in range(joint.num_positions()):
+                names[joint.position_start() + i] = \
+                    f"{joint.name()}_{joint.position_suffix(i)}"
+    for ind in mbp.GetFloatingBaseBodies():
+        body = mbp.get_body(ind)
+        start = body.floating_positions_start()
+        for i in range(7):
+            names[start
+                  + i] = f"{body.name()}_{body.floating_position_suffix(i)}"
+    return namedview(view_name, names)
+
+
+def MakeNamedViewVelocities(mbp,
+                            view_name,
+                            add_suffix_if_single_velocity=False):
+    names = [None] * mbp.num_velocities()
+    for ind in range(mbp.num_joints()):
+        joint = mbp.get_joint(JointIndex(ind))
+        if joint.num_velocities() == 1 and not add_suffix_if_single_velocity:
+            names[joint.velocity_start()] = joint.name()
+        else:
+            for i in range(joint.num_velocities()):
+                names[joint.velocity_start() + i] = \
+                    f"{joint.name()}_{joint.velocity_suffix(i)}"
+    for ind in mbp.GetFloatingBaseBodies():
+        body = mbp.get_body(ind)
+        start = body.floating_velocities_start() - mbp.num_positions()
+        for i in range(6):
+            names[start
+                  + i] = f"{body.name()}_{body.floating_velocity_suffix(i)}"
+    return namedview(view_name, names)
+
+
+def MakeNamedViewState(mbp, view_name):
+    # TODO: this could become a nested named view, pending
+    # https://github.com/RobotLocomotion/drake/pull/14973
+    pview = MakeNamedViewPositions(mbp, f"{view_name}_pos", True)
+    vview = MakeNamedViewVelocities(mbp, f"{view_name}_vel", True)
+    return namedview(view_name, pview.get_fields() + vview.get_fields())
