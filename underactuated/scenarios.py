46405ce1bd920e8e4f37b3df9ad1de004c664c34
Russ Tedrake <russt@mit.edu>
2022-05-22 12:35:29 -0400

    Add 3D quadrotor with MBP example

--- underactuated/scenarios.py
+++ underactuated/scenarios.py
@@ -3,7 +3,8 @@ This file contains a number of helper utilities to set up our various
 experiments with less code.
 """
 import numpy as np
-from pydrake.all import (Box, CoulombFriction, Cylinder, RigidTransform,
+from pydrake.all import (BallRpyJoint, Box, CoulombFriction, Cylinder,
+                         PrismaticJoint, RevoluteJoint, RigidTransform,
                          SpatialInertia, Sphere, UnitInertia)
 
 
@@ -49,3 +50,36 @@ def AddShape(plant, shape, name, mass=1, mu=1, color=[.5, .5, .9, 1.0]):
         plant.RegisterVisualGeometry(body, RigidTransform(), shape, name, color)
 
     return instance
+
+
+# https://github.com/RobotLocomotion/drake/issues/14949
+def AddFloatingRpyJoint(plant, frame, instance, use_ball_rpy=True):
+    inertia = SpatialInertia(mass=0,
+                             p_PScm_E=[0., 0., 0.],
+                             G_SP_E=UnitInertia(0, 0, 0))
+    x_body = plant.AddRigidBody("x", instance, inertia)
+    plant.AddJoint(
+        PrismaticJoint("x", plant.world_frame(), x_body.body_frame(),
+                       [1, 0, 0]))
+    y_body = plant.AddRigidBody("y", instance, inertia)
+    plant.AddJoint(
+        PrismaticJoint("y", x_body.body_frame(), y_body.body_frame(),
+                       [0, 1, 0]))
+    z_body = plant.AddRigidBody("z", instance, inertia)
+    plant.AddJoint(
+        PrismaticJoint("z", y_body.body_frame(), z_body.body_frame(),
+                       [0, 0, 1]))
+    if use_ball_rpy:
+        plant.AddJoint(BallRpyJoint("ball", z_body.body_frame(), frame))
+    else:
+        # RollPitchYaw is body z-y-x
+        rz_body = plant.AddRigidBody("rz", instance, inertia)
+        plant.AddJoint(
+            RevoluteJoint("rz", z_body.body_frame(), rz_body.body_frame(),
+                          [0, 0, 1]))
+        ry_body = plant.AddRigidBody("ry", instance, inertia)
+        plant.AddJoint(
+            RevoluteJoint("ry", rz_body.body_frame(), ry_body.body_frame(),
+                          [0, 1, 0]))
+        plant.AddJoint(
+            RevoluteJoint("rx", ry_body.body_frame(), frame, [1, 0, 0]))
