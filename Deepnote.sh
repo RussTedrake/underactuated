303bb6174fd63a7f57c3227ddbb1e661f88f1cd5
Russ Tedrake <russt@mit.edu>
2023-02-06 08:35:04 -0500

    move deepnote commands to sh file instead of md file, so that if one fails, I don't rewrite a bad sha to deepnote

--- Deepnote.sh
+++ Deepnote.sh
@@ -1,13 +1,9 @@
-# For developers
+#!/bin/bash
 
-See manipulation/Deepnote.md for most details.  Here are the relevant commands for updating the underactuated repos:
-```
 docker pull robotlocomotion/drake:focal
 docker build -f setup/docker/Dockerfile -t russtedrake/underactuated:latest .
 docker push russtedrake/underactuated:latest
 docker build -f setup/docker/Dockerfile -t russtedrake/underactuated:$(git rev-parse --short HEAD) .
 docker push russtedrake/underactuated:$(git rev-parse --short HEAD)
-echo russtedrake/underactuated:$(git rev-parse --short HEAD)
 python3 htmlbook/publish_to_deepnote.py $(git rev-parse --short HEAD)
 echo "Remember to log on to deepnote and build the dockerfile in any one of the notebooks"
-```
