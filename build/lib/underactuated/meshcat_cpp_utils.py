0d4fff51a8de87bf0c6d5de4b62a21be02bdabf3
Sunbochen Tang <{ID}+{username}@users.noreply.github.com>
2023-02-23 10:53:33 -0500

    remove local untracked files that was pushed earlier

--- build/lib/underactuated/meshcat_cpp_utils.py
+++
@@ -1,8 +0,0 @@
-import warnings
-
-from underactuated.meshcat_utils import *
-
-warnings.warn(
-    "underactuated.meshcat_cpp_utils has been renamed to underactuated.meshcat_utils. This shim will be removed after 2023-05-31.",
-    DeprecationWarning,
-    stacklevel=2)
