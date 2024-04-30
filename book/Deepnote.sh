2e17c900577636dde45707e321378aee560ea337
Russ Tedrake <russt@mit.edu>
2024-04-30 09:07:34 -0400

    initial draft of polytope dynamics / tube MPC

--- book/Deepnote.sh
+++ book/Deepnote.sh
@@ -3,5 +3,3 @@
 set -e
 
 python3 htmlbook/publish_to_deepnote.py $(cat Deepnote_docker_sha.txt)
-echo "Remember to log on to deepnote and build the dockerfile in any one of the notebooks"
-echo "https://deepnote.com/workspace/$(cat Deepnote_workspace.txt)"
--- book/Deepnote_docker.sh
+++ book/Deepnote_docker.sh
@@ -8,3 +8,6 @@ docker push russtedrake/underactuated:latest
 docker build -f setup/docker/Dockerfile -t russtedrake/underactuated:$(git rev-parse --short HEAD) .
 docker push russtedrake/underactuated:$(git rev-parse --short HEAD)
 git rev-parse --short HEAD > book/Deepnote_docker_sha.txt
+echo "Remember to run Deepnote.sh to actually push to Deepnote"
+echo "Remember to log on to deepnote and build the dockerfile in any one of the notebooks"
+echo "https://deepnote.com/workspace/$(cat Deepnote_workspace.txt)"
